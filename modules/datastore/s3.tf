544a435e429c11aa49303e974cf6cb917f842f5f
Ed Smith <esmith@roboticresearch.com>
2024-07-02 11:14:38 -0400

    Update s3 config

--- modules/datastore/s3.tf
+++ modules/datastore/s3.tf
@@ -1,15 +1,7 @@
 resource "aws_s3_bucket" "this" {
-  bucket        = local.s3_bucket_name
-  acl           = "private"
+  bucket = local.s3_bucket_name
+
   force_destroy = var.force_destroy_s3_bucket
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.s3.arn
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
 
   tags = merge(
     var.standard_tags,
@@ -19,6 +11,21 @@ resource "aws_s3_bucket" "this" {
   )
 }
 
+resource "aws_s3_bucket_acl" "this" {
+  bucket = aws_s3_bucket.this
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+      kms_master_key_id = aws_kms_key.s3.arn
+    }
+  }
+
+}
 resource "aws_s3_bucket_public_access_block" "this" {
   bucket = aws_s3_bucket.this.id
 
