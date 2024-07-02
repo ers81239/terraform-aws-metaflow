6abda09afb0a684077ced025531d7af12c7838b6
Ed Smith <esmith@roboticresearch.com>
2024-07-02 11:38:29 -0400

    Update s3 config

--- modules/datastore/s3.tf
+++ modules/datastore/s3.tf
@@ -12,12 +12,12 @@ resource "aws_s3_bucket" "this" {
 }
 
 resource "aws_s3_bucket_acl" "this" {
-  bucket = aws_s3_bucket.this
+  bucket = aws_s3_bucket.this.bucket
   acl    = "private"
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
-  bucket = aws_s3_bucket.this
+  bucket = aws_s3_bucket.this.bucket
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm     = "aws:kms"
